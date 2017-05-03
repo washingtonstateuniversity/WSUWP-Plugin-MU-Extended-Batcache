6007f9c003a9274befb305bfcdd90b4b15336381
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-03 15:38:21 -0700

    Initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+# WSUWP MU Extended Batcache.
+
+A fork and extension of the batcache.php file included with Batcache.
+
+* Clears home and single `$post` URL page cache when content is updated.
+* Clears REST API page cache when content is updated.
---
+++ wsuwp-mu-extended-batcache.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WSU MU Extended Batcache
+Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Plugin-MU-Extended-Batcache
+Description: A fork and extension of the batcache.php file included with Batcache.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: https://web.wsu.edu
+Version: 2.0.0
+*/
